ec0677e714caa4b6a6111ba4d56b9e36e4906e2e
Shaik khadar vali <skkhadarvali74@gmail.com>
2026-01-07 17:04:51 +0530

    Add files via upload
    data types, varibles and i/o functions

---
+++ day1.py
@@ -0,0 +1,3 @@
+print("my self khadar vali")
+print("python day 1 in codegnan")
+print("i am performing some output operations")
---
+++ day2.py
@@ -0,0 +1,17 @@
+#data types and I/O functions
+name="khadar vali"#string
+age=25 #integer
+weight=98.55  #float datatype
+list1=[1,2,3,'hi',2,4,(10,32)]#list of items
+tuple1=(10,20,30,'welcome','hello',) #tuple of items
+cards={'a',2,3,4,5,6,7,8,9,10,'j','k','q',2,3,4} #pack of cards in set data type
+student_marks={"maths":60,"telugu":98} #dictionary
+
+print("my name is:",name)
+print("my age is:",age)
+print("my weight:",weight)
+print("list of items:",list1)
+print("tuple of items:",tuple1)
+print("card values:",cards)
+print("student subject marks:",student_marks)
+    
