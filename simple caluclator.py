928dcf7a6f5d197f00e202143057cc879a724704
Shaik khadar vali <skkhadarvali74@gmail.com>
2026-01-08 19:01:14 +0530

    day 3 simple calculator

---
+++ simple caluclator.py
@@ -0,0 +1,12 @@
+#simple caluclator operations
+#variables and operators
+
+a=2
+b=3
+print("sum of two numbers is:",a+b)
+print("substraction of two numbers is:",b-a)
+print("multiplication of two numbers is:",a*b)
+print("divison of two numbers is :",b/a)
+print("floor devision of two numbers:",b//a)
+print("modulo division of two numbers:",b%a)
+print("power of two numbers is:",b**a)
