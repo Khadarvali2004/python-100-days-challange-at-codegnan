5f63d0cec025e850a7dfd7e51a57488deb437ce2
Shaik khadar vali <skkhadarvali74@gmail.com>
2026-01-09 16:59:18 +0530

    Add files via upload

---
+++ day 3.2 operators (2).py
@@ -0,0 +1,16 @@
+#Relational / Comparision Operators
+a=8
+b=5
+print("Equal to:", a==b)        # Equal to
+print("Not equal to:",a!=b)    # Not equal to
+print("Greater than:", a>b)     # Greater than
+print("Less than:",a<b)        # Less than
+print("Greater than or equal to:",a>=b)    # Greater than or equal to
+print("Less than or equal to:", a<=b)       # Less than or equal to
+# Logical operators
+value1 = 5
+value2 = 7
+
+print("Logical AND:", value1>10 and value2<10) #AND
+print("Logical OR:", value1>10 or value2<10)    #OR
+print("Logical NOT:", not value1>10)            #NOT
---
+++ day 3.2 operators.py
@@ -0,0 +1,53 @@
+#Assignment Operators
+print("= = = ASSIGNMENT OPERATORS = = =")
+num1= 10
+
+num1 += 1 
+print("Addition Equals to : ",num1)           #Addition Equals to
+num1 -= 1
+print("Subtraction Equals to : ", num1)         #Subtraction Equals to
+num1 *= 2
+print("Multiplication Equals to : ", num1)   #Multiplication Equals to
+num1 /= 2
+print("Division Equals to : ", num1)             #Division Equals to
+num1 %= 4
+print("Modulus equal to : ", num1)            #Modulus equal to
+num1 **= 3
+print("Exponential Equal to : ", num1)      #Exponential Equal to
+num1 //= 3
+print("Floor Division Equals to : ", num1)  #Floor Division Equals to
+
+#Bitwise Operations
+x=5
+y=3
+
+print (" AND (&) is:",(x&y))        # AND (&)
+print (" OR (|) is:",(x|y))             # OR (|)
+print (" XOR (^) is:",(x^y))          # XOR (^)
+print (" Not (~) is:",(~x))            # Not (~)
+print (" Shift Right(>>) is:",(x>>1))   # Shift Right(>>)
+print (" Shift Left (<<) is:",(x<<2))    # Shift Left (<<)
+
+# Membership operators
+fruits = ["apple", "banana","khadar", "cherry"]
+
+print("Membership (in):", "ramya" in fruits)    # Membership - in
+print("Membership (not in):", "khadar" not in fruits)     # Membership - not in
+
+#Identity operators
+x = "khadar"
+y = 5
+
+print("Identity (is):", x is y)   # Identity - is
+print("Identity (is not):", x is not y)     # Identity - is not
+
+
+
+
+
+
+
+
+
+
+
