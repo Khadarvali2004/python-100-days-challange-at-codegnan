9bfb74cfc883836d71f5f3dd4775f7b9a2ee8720
Shaik khadar vali <skkhadarvali74@gmail.com>
2026-01-12 17:31:57 +0530

    nested loop statements

---
+++ day 6 nested loop statements.py
@@ -0,0 +1,46 @@
+#nested if condition 
+"""n=int(input())
+
+if n%2==0:
+    if n>-1:
+        print("number is +ve even")
+    else:
+        print("number is -ve even")
+else:
+    if n>-1:
+        print("number is +ve odd")
+    else:
+        print("number is -ve odd")"""
+
+
+#check which one is even number or odd number in the list of numbers
+
+"""list=[2,4,3,5,6,7,9]
+
+for num in list:
+    if num%2==0:
+        print(num,"is even")
+    else:
+        print(num,"is odd")
+print("program done")"""
+
+#check ehich number is +ve and -ve in the given list
+"""list=[2,4,-3,5,-6,-7,9]
+for num in list:
+    if num>=0:
+        print(num,"is positive")
+    else:
+        print(num,"is negative")
+    print("program done")"""
+
+#print 1 to 20 numbers using range function
+
+"""for num in range(1,21,1):
+    print(num)"""
+#print 2 to 30 numbers using range function
+
+for num in range(2,30,2):
+    print(num)
+
+    
+
