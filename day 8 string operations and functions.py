858c885b31fd8ff46564eecffc3401432c34bf93
Shaik khadar vali <skkhadarvali74@gmail.com>
2026-01-21 22:24:20 +0530

    day8 string operation and functions

---
+++ day 8 string operations and functions.py
@@ -0,0 +1,46 @@
+#concatination
+"""s1 ="code"
+s2 ="gnan"
+s3 =s1+s2
+print(s3)"""
+#string repeat
+"""s ="codegnan"
+repetation =3
+print(s*repetation)"""
+#string function
+"""s = "i am python batch student"
+print(s.lower())
+print(s.upper())
+print(s.title())
+print(s.swapcase())
+print(s.capitalize())"""
+#substrin index findining
+"""s ="i am python batch student"
+print(s.find("t"))"""
+#strip() it removes the white space
+"""s="@@@@@cod@egnan@@@@@"
+print(s)
+print(s.strip('@'))
+print(s.strip())
+print(s.rstrip())"""
+#
+"""s1 ="code"
+s2 ="gnan"
+s3 ="123"
+s4 ="abc123"
+s5 ="codegnan"
+print(s1.isupper())
+print(s2.isupper())
+print(s1.islower())
+print(s2.islower())
+print(s3.isdigit())
+print(s4.isalnum())
+print(s5.isalpha())"""
+
+
+
+
+
+
+
+
