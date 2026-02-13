dab590188bf7507697d01049840cff39e726c48f
Shaik khadar vali <skkhadarvali74@gmail.com>
2026-02-13 13:48:43 +0530

    Add files via upload
    Type converstions and i/o operations

---
+++ day 2 type converstions and operations.py
@@ -0,0 +1,28 @@
+# read input numbers from users
+"""num =input()
+print(type(num))#checking the type of the number
+print(num)"""
+#implicit type converstion
+"""num1=int(input())
+val=float(input())
+print(type(num1),type(val))#checking type of number
+res = num1+val
+print(res)"""
+
+"""num1=input() #read string type number
+print(type(num1))#checking the type of number
+res = num1+10
+print(res)"""
+
+#to overcome above problem by using exxplicit type converstion
+"""
+num1=input() #read string type number
+print(type(num1))#checking the type of number
+res = int(num1)+10
+print(res)
+"""
+#converting integer to float and boolean
+num=10.0
+print(int(num))
+print(str(num))
+print(bool(num))
