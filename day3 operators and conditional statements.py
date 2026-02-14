0d6473192d05c0b20902b3a5e89f642ae5811e8e
Shaik khadar vali <skkhadarvali74@gmail.com>
2026-02-14 19:28:54 +0530

    Add files via upload

---
+++ day3 operators and conditional statements.py
@@ -0,0 +1,29 @@
+"""#check number is zero
+n=int(input())
+if n==0:
+    print("number is zero")
+
+print("program is done")"""
+"""#check number is zero or not
+n=int(input())
+if n==0:
+    print("number is zero")
+else:
+    print("number is not zero")
+print("well done")"""
+"""
+#number is not equal to zero
+n=int(input())
+if n!=0:
+    print("number is not zero")
+else:
+    print("number is zero")
+print("well done")    """
+
+#check the given number is big number or not
+a=10
+b=20
+if a>b:
+    print("a is bigger")
+else:
+    print("b is bigger")
