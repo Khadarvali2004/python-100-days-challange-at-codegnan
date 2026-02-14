f0c22ee569d08ec9beb8a1623f03ea4e8a35bdc6
Shaik khadar vali <skkhadarvali74@gmail.com>
2026-02-14 09:12:22 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,41 @@
+#converting integer into string and float and bool
+"""
+num=15
+print(type(num))
+print(float(num))
+print(bool(num))"""
+
+#converting string into integer and float and bool
+"""
+text="25"
+print(int(text))
+print(float(text))
+print(bool(text))
+print(tuple(text))
+print(list(text))
+print(set(text))"""
+
+#converting list into string set and tuple
+"""
+my_list=[10,20,30,48]
+print(tuple(my_list))
+print(set(my_list))
+print(str(my_list))"""
+
+#converting tuple into list set and string
+"""
+my_tuple=('2','5','6','5')
+print(list(my_tuple))
+print(set(my_tuple))
+print(str(my_tuple))
+"""
+"""#converting set into list tuple and string
+my_set={'10','20','33','10'}
+print(list(my_set))
+print(tuple(my_set))
+print(str(my_set))"""
+
+
+
+
+
