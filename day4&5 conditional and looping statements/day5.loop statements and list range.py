1dcdfb4151fab67501f6a67d55a635baa8a941f9
Shaik khadar vali <skkhadarvali74@gmail.com>
2026-02-19 17:42:17 +0530

    Add files via upload

---
+++ day4&5 conditional and looping statements/day5.loop statements and list range.py
@@ -0,0 +1,26 @@
+"""#find which is even or odd from list numbers
+lst=[1,2,3,4,5]
+for ind in range(0,5,1):
+    if lst[ind]%2==0:
+        print(lst[ind],"is even")
+    else:
+        print(lst[ind],"is odd")"""
+#loops
+#1.print even numbers between 20-40
+"""for i in range(20,41,2):
+    print(i)"""
+"""for num in range(20,41,2):
+    if num%2==0:
+        print(num)"""
+"""
+#numbers between 1 to 100
+for i in range(1,101,1):
+    print(i,end=" ")"""
+"""#find the sum of n natural numbers
+n=int(input())
+total=0
+for num in range(1,n+1):
+    total=total+num
+print(total)"""
+
+
