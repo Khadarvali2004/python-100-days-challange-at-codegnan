c9a3a8dc1b96a16bcd011e8046fa103152a1e00c
Shaik khadar vali <skkhadarvali74@gmail.com>
2026-02-23 21:47:38 +0530

    Add files via upload

---
+++ day6 while loops.py
@@ -0,0 +1,19 @@
+#student marks
+"""n = int(input("Enter number of subjects: "))
+i = 1
+highest = 0
+
+while i <= n:
+    marks = int(input("Enter marks: "))
+    if marks > highest:
+        highest = marks
+
+    i += 1
+print("Highest marks =", highest)"""
+#multiplication table
+n = int(input("Enter a number: "))
+i = 1
+
+while i <= 20:
+    print(n, "x", i, "=", n * i)
+    i += 1
